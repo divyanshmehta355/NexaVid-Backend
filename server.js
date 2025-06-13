bc0fb3ba6b2d05f42a615deeef0420fec904ba34
divyanshmehta355 <devmehta1749@gmail.com>
2025-06-13 12:52:45 +0530

    ⚙️ Port Config

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 
-import authRoutes from './routes/authRoutes.js';
-import videoRoutes from './routes/videoRoutes.js';
+import authRoutes from "./routes/authRoutes.js";
+import videoRoutes from "./routes/videoRoutes.js";
 
 dotenv.config();
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(cors());
-app.use(express.json({ limit: '20mb' }));
+app.use(express.json({ limit: "20mb" }));
 
-app.use('/api/auth', authRoutes);
-app.use('/api/videos', videoRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/videos", videoRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('MongoDB connected');
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
+    console.log("MongoDB connected");
+    app.listen(port, () => console.log(`Server running on port ${port}...`));
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
